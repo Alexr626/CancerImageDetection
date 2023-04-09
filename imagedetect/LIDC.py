34de26e3ec5abf2ac74bf63b937d802507cf48d8
anamhira <hira.anam49@gmail.com>
2023-04-09 15:59:53 -0700

    fds

--- imagedetect/LIDC.py
+++ imagedetect/LIDC.py
@@ -78,8 +78,8 @@ def Lidc2Voxel(out_dir):
     f = open(out_dir + '/labels.csv', 'w')
     f.write('id,malignancy,diameter,malignancy_th,testing,x,y,z\n')
     # load in train patients and test
-    train_patients = np.load('train_patients.csv')
-    for c, (case,nodule, malignancy, diameter, malignancy_th,(x,y,z)) in enumerate(nodules):
+    train_patients = np.load('Data/Meta/patient_id_train_list.csv')
+    for c, (case, nodule, malignancy, diameter, malignancy_th,(x,y,z)) in enumerate(nodules):
         if train_patients == 1:
             testing = 0
         else:
