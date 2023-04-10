e7bc4ea1439a1486f60f6d54b721de5c07605263
anamhira <hira.anam49@gmail.com>
2023-04-09 21:44:51 -0700

    fds

--- imagedetect/trainer.py
+++ imagedetect/trainer.py
@@ -128,7 +128,8 @@ class Trainer:
 
     def calc_accuracy(self, x, y):
         # Check that the argmax of softmax is the same as the target
-        print("x_valid" + x)
+        print("x_valid")
+        print(x)
         print(y)
         print(x.shape)
         print(y.shape)
